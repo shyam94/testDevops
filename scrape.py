e4790b5d5dbfd2030a5b7a2a0b9943d1784f283e
DvssPavan <43805820+DvssPavan@users.noreply.github.com>
2021-07-01 00:45:17 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,17 +1,42 @@
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as soup
 import re
+
+
+# In[82]:
+
+
 page = urlopen("https://docs.aws.amazon.com/AmazonS3/latest/API/API_Operations_Amazon_Simple_Storage_Service.html")
-soup = BeautifulSoup(page, "html.parser")
-apiEndpoints = soup.find_all('li',{'class':'listitem'})
+
+
+# In[83]:
+
+
+beautsoup = soup(page, "html.parser")
+apiEndpoints = beautsoup.find_all('li',{'class':'listitem'})
+
+
+# In[84]:
+
+
 for endpoint in apiEndpoints:
-    eachPageName = endpoint.a["href"][2:]
+    eachPageName = endpoint.a["href"][2:len(endpoint.a["href"])-5]
+    attributeValue='#'+eachPageName+'_ResponseSyntax'
+    #print(attributeValue)
     #print("https://docs.aws.amazon.com/AmazonS3/latest/API/"+eachPageName)
     eachPageURI = "https://docs.aws.amazon.com/AmazonS3/latest/API/"+eachPageName
+    print(eachPageURI)
     eachPage = urlopen(eachPageURI)
     #print(eachPage)
-    nestedPageSoup = BeautifulSoup(eachPage, "html.parser")
-    #print(nestedPageSoup)
+    nestedPageSoup = soup(eachPage, "html.parser")
+    responseHeading=nestedPageSoup.find_all('a',{'href':attributeValue})
+    #print(responseHeading)
+    for eachColumn in responseHeading:
+        #eachColumn= item.a
+        print(eachColumn.get_text())
+    #responseHeading=nestedPageSoup.find_all("a", href ="#API_CompleteMultipartUpload_ResponseSyntax")
+    #print(responseHeading)
     #print(nestedPageSoup.find_all(re.compile("\w")))
-    responseColumns = nestedPageSoup.find_all('div',{'class':'variablelist'})
-    print(responseColumns[1])
+    #responseColumns = nestedPageSoup.find_all('div',{'class':'variablelist'})
+    #print(responseColumns[1])

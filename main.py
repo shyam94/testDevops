fb0f43af0e8aaa0ba3f6c9975f0a2d1f2e3205b0
shyam94 <38715135+shyam94@users.noreply.github.com>
2021-09-07 20:51:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -204,7 +204,7 @@ def update_Job_Id(url):
 
 def run_bamboo_adapter_build(input_args: dict):
     print("Building driver/adapter on bamboo...BEGIN")
-    bamboo_build = DrMemoryTask(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], input_args)
+    bamboo_build = DrMemoryTask(sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], r"\"" + sys.argv[6] + "\"", input_args)
     projectKeys = ["TSTFOMEM"]
     if not bamboo_build.excludeCompile:
         projectKeys.insert(0,"BULDOMEM")

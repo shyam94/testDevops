a51f09040bdb319634d95782e403c2e86816e505
shyam94 <38715135+shyam94@users.noreply.github.com>
2021-09-13 12:26:51 +0530

    Update MetaTestRunner.py

--- MetaTestRunner.py
+++ MetaTestRunner.py
@@ -200,4 +200,4 @@ def main(inUserName: str, inPassword: str, inputFileName: str):
 
 
 if __name__ == '__main__':
-    main(sys.argv[0], sys.argv[1], sys.argv[2])
+    main(sys.argv[1], sys.argv[2], sys.argv[3])
